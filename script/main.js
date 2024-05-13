5791f3125945e0bfc689c4eb1fbc1795e0aff92f
Mhtrobot <mhtrobot760@gmail.com>
2024-05-13 23:05:11 +0330

    js init

--- script/main.js
+++ script/main.js
@@ -1,5 +1,11 @@
 //variables/classes-----
-
+let title = document.querySelector('.title .message').textContent,//for titling the game result situation
+    score = document.querySelector('.score').textContent,//for changing the score countdown
+    turn = document.querySelector('.turn').textContent,//showing the turn and the result of computer choose
+    choose = document.querySelector('.choose'),//player choice
+    button = document.querySelector('.c-btn'),//hitting the button
+    playerScore = 0,
+    computerScore = 0;
 //Event listeners---------
 
 //functions--------------
